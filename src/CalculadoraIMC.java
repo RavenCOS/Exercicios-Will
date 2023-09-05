1df3d61652e976e888255f19bc4960fb01c0eb46
Marcos Castro <marcoscastro650@gmail.com>
2023-09-05 12:35:59 -0300

    Correções - Part 3

--- src/CalculadoraIMC.java
+++ src/CalculadoraIMC.java
@@ -5,7 +5,6 @@ public class CalculadoraIMC {
     static double peso;
     static double altura;
     static double imc;
-    static String classificacao;
 
     static Scanner teclado = new Scanner(System.in);
 
@@ -15,22 +14,21 @@ public class CalculadoraIMC {
 
     public static void coletaAltura() {
         altura = teclado.nextDouble();
-        calculoIMC();
+
     }
 
     public static void calculoIMC() {
         imc = peso / Math.pow(altura, 2);
-        classificacao();
     }
 
-    public static void classificacao() {
+    public static String calculoClassificacao() {
         String resultado = imc < 18.5 ? "Magreza" : "erro";
         resultado = imc >= 18.5 && imc <= 24.9 ? "Normal" : resultado;
         resultado = imc >= 25 && imc <= 29.9 ? "Sobrepeso" : resultado;
         resultado = imc >= 30 && imc <= 34.9 ? "Obesidade grau I" : resultado;
         resultado = imc >= 35 && imc <= 39.9 ? "Obesidade grau II" : resultado;
         resultado = imc >= 40 ? "Obesidade grau III" : resultado;
-        classificacao = resultado;
+        return resultado;
     }
 
     public static void main(String[] args) {
@@ -41,6 +39,8 @@ public class CalculadoraIMC {
         coletaPeso();
         System.out.print("Digite sua altura: ");
         coletaAltura();
+        calculoIMC();
+        String classificacao = calculoClassificacao();
 
         System.out.printf("Com um Imc de %.1f você se encontra na classificacao: %s", imc, classificacao);
         teclado.close();
